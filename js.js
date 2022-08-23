d83dba3bd7a4072ca7237ff31c48c4a8388cabfb
Khemendra Bhardwaj <khemendra123b@gmail.com>
2022-08-23 16:09:19 +0530

    all files add to staging area

---
+++ js.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+// const request = require("request");
+app.use(express.static("public"));
+// all css files images pdf  are static
+//use above code to access them   
+app.get("/", function(req,res)
+{ res.sendFile(__dirname+"/html.html");
+
+
+  console.log("file send successfully");
+}
+);
+
+
+app.listen(3000, function(){
+  console.log("port working on port 3000");
+}
+
+);
